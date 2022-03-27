7b4e856403c05321576f5a16a640a95f37dbdf6c
cdorais1 <86804682+cdorais1@users.noreply.github.com>
2022-03-27 16:12:18 -0400

    Update DataManager.js

--- source/DataManager.js
+++ source/DataManager.js
@@ -1,1 +1,15 @@
 import * as cornerstone from "cornerstone-core";
+
+// This class is a data container and will not have any methods of its own, only variables.
+class dataManager
+{
+    // Create a constructor with dummy variables to list out necessary information gathered from the passed in DICOM.
+    constructor(dicom)
+    {
+        this._instanceID = 0;
+        this._imageDepth = 0;
+        this._imageXSize = 0;
+        this._imageYSize = 0;
+    }
+
+}
