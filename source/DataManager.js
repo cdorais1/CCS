617de447a091fe48250d6ba6e3080c262174d2bf
sablejr <sablejr@gmail.com>
2022-03-28 18:57:49 -0400

    added event listeners (one to click on button, one for test, not fully working yet), html to make them work

--- source/DataManager.js
+++ source/DataManager.js
@@ -1,14 +1,34 @@
 import * as cornerstone from "cornerstone-core";
 import assert from 'assert';
 
+const realInputButton = document.getElementById("user-file-input");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function() {
+    realInputButton.click();
+
+});
+
+realInputButton.addEventListener("change", function() {
+    if (realInputButton.value) {
+        if (realFileBtn.value) {
+            customTxt.innerHTML = realFileBtn.value;
+        } else {
+            customTxt.innerHTML = "No file chosen, yet"
+        }
+    }
+
+});
+
 // This class is a data container and will not have any methods of its own.
 class dataManager
 {
-    // Create a constructor with dummy variables to list out necessary information gathered from the passed in DICOM; subject to change.
+    // Create a constructor with dummy variables to list out necessary PICTURE information gathered from the passed in DICOM; subject to change.
     constructor(dicom)
     {
         //Is this a DICOM file?, if not print "Unable to read, please insert a DICOM file" *need to make a 'input' variable
-        console.assert(input = dicom, "Unable to read, please insert a DICOM file");
+        console.assert(input == dicom, "Unable to read, please insert a DICOM file");
 
         if (!dataManager.instance)
         {
@@ -18,6 +38,7 @@ class dataManager
             this._imageDepth = [];
             this._imageXSize = [];
             this._imageYSize = [];
+
         }
 
         return dataManager.instance;
