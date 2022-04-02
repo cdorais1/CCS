1036170a9fb7375727bc52a6e33088224d249f2a
cdorais1 <cdorais1@gmail.com>
2022-04-02 12:56:42 -0400

    minor fix in SecondPage.html, updated DataManager.js, and commented steps in util.js.

--- source/DataManager/DataManager.js
+++ source/DataManager/DataManager.js
@@ -1,6 +1,6 @@
 // This class is a data container and will not have any methods of its own.
-require(dataSet);
-
+import dataSet from '../../node_modules/dicom-parser/';
+import ; 
 export class dataManager
 {
     constructor(dataSet)
@@ -12,5 +12,7 @@ export class dataManager
         this.patientID = dataSet.string('x00100020'); // Patient id 
         this.sopInstanceUid = dataSet.string('x0020000d') // Unique id of DICOM/image
         this.seriesUid = dataSet.string('x0020000e') // id of image in series. 
+        this.imageID;
+        this.pixelData;
     }
 }
